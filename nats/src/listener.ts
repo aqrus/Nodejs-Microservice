02dcfb4eb637c007453bb031cc5dd4abe0581ac0
tgbao <truonggiabao12345@gmail.com>
2022-07-20 16:48:14 +0700

    update code

--- nats/src/listener.ts
+++ nats/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 import { TicketCreatedListener } from './events/tickets';
 console.clear();
@@ -12,21 +12,7 @@ stan.on('connect', () => {
         console.log('NATS connection closed');
         process.exit();
     });
-    const options = stan
-        .subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable()
-        .setDurableName('my-durable');
-    const subcription = stan.subscribe('ticket:created',options);
-
-    subcription.on('message', (msg: Message) => {
-        const data = msg.getData();
-        if(typeof data === 'string'){
-            const sequence = msg.getSequence();
-            const dataMsg = JSON.parse(data);
-            console.log('Received event:', sequence, dataMsg);
-        }
-    });
+    new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
